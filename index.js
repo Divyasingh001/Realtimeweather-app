59783463ae6a32f421683ee0a3eb439ed415d54d
Divya Singh <102237112+Divyasingh001@users.noreply.github.com>
2024-09-22 21:07:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+
+// - Local:    http://localhost:3000       │
+// │   - Network:  http://192.168.0.112:3000 
